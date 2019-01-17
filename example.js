496e53dd59720e966a426536212dca5dfccb640d
Byron Varberg <byron@byronvarberg.com>
2019-01-17 00:09:07 -0600

    Clean up example

--- example.js
+++ example.js
@@ -3,21 +3,23 @@ const Bonfhir = require('./');
 class Application {
   static async main() {
     const fhir = Bonfhir.createClient({
-      version: '1.0.2',
       serviceRootURL: 'https://r3.smarthealthit.org',
     });
 
-    const searchResult = await fhir.search({
-      type: 'Condition',
-      parameters: {
-        patient: 'Patient/7b1b9efd-50a4-4b8b-a772-a24aafe01f7f',
-      },
-    });
+    try {
+      const searchResult = await fhir.search({
+        type: 'Condition',
+        parameters: {
+          patient: 'Patient/7b1b9efd-50a4-4b8b-a772-a24aafe01f7f',
+        },
+      });
 
-    console.log(searchResult);
+      console.log(searchResult);
+    } catch (err) {
+      console.log(err);
+    }
+
   }
 }
 
-Application.main().catch((err) => {
-  console.error(err);
-});
+Application.main();
